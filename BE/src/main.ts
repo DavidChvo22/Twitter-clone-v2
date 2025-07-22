c70dd20a834e8f89b33e78122da55f2e8ea003ac
DavidChvo22 <david.chvostal@gmail.com>
2025-07-22 21:30:39 +0200

    Debug cleanup

--- BE/src/main.ts
+++ BE/src/main.ts
@@ -4,8 +4,6 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-console.log('Current Working Directory is:', process.cwd());
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
