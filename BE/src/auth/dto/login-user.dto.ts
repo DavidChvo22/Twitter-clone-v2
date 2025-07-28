182bcd1958793604d814644581ba7748797cc068
DavidChvo22 <62336983+DavidChvo22@users.noreply.github.com>
2025-07-28 21:26:06 +0200

    login dto conditions removed

--- BE/src/auth/dto/login-user.dto.ts
+++ BE/src/auth/dto/login-user.dto.ts
@@ -11,28 +11,17 @@ export class LoginUserDto {
   @ApiProperty({
     description: 'Používateľské meno',
     example: 'JohnDoe',
-    minLength: 3,
-    maxLength: 16,
-  })
-  @Matches(/^[A-Z]/, {
-    message: 'Username must start with a capital letter',
   })
   @IsString()
   @IsNotEmpty()
-  @MinLength(3)
-  @MaxLength(16)
   username: string;
 
   @ApiProperty({
     description: 'Heslo',
     example: 'password123',
-    minLength: 6,
-    maxLength: 100,
   })
   @IsString()
   @IsNotEmpty()
-  @MinLength(6)
-  @MaxLength(100)
   password: string;
 }
 
